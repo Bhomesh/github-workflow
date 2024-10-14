d054739d3b3e9055acfb19bb69731f6b7d600ba9
Bhomesh <bhomeshrazdan@gmail.com>
2024-10-14 16:03:46 +0530

    update eslint.yml

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,9 @@
+/* global test expect */
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+
 
 test('renders learn react link', () => {
   render(<App />);
